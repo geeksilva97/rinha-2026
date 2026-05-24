dace871488b4992e29cec2da4d84cff1b79eb71c
geeksilva97 <edigleyssonsilva@gmail.com>
2026-05-23 22:31:00 -0300

    ivf implemented

--- native/k_means.cpp
+++ native/k_means.cpp
@@ -6,16 +6,16 @@
 #include <random>
 
 static_assert(std::endian::native == std::endian::little,
-                "this code assumes little-endian");
+              "this code assumes little-endian");
 
 using namespace std;
 using json = nlohmann::json;
 
 std::mt19937 rng(42); // seed
 
-#define K 1700
-#define T 20
-#define DIM 14
+constexpr uint32_t K = 1700;
+constexpr uint32_t T = 20;
+constexpr uint32_t DIM = 14;
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
@@ -27,16 +27,19 @@ int main(int argc, char *argv[]) {
   std::ifstream f(path);
   json data = json::parse(f);
   std::vector<std::vector<float>> points;
+  std::vector<uint8_t> labels;
   points.reserve(data.size());
-  for (auto &item : data)
+  labels.reserve(data.size());
+  for (auto &item : data) {
     points.push_back(item["vector"]);
+    labels.push_back(item["label"] == "fraud" ? 1 : 0);
+  }
 
   std::vector<size_t> assignments(data.size());
-  // std::iota(indices.begin(), indices.end(), 0);
-  // std::vector<size_t> centroids;
-  //
   std::vector<std::vector<float>> centroids(K);
   std::sample(points.begin(), points.end(), centroids.begin(), K, rng);
+  std::vector<std::vector<float>> sum(K, std::vector<float>(DIM, 0.0f));
+  std::vector<int> count(K, 0);
 
   for (int t = 0; t < T; ++t) {
     // ASSIGN
@@ -63,9 +66,9 @@ int main(int argc, char *argv[]) {
     }
 
     // UPDATE
-    std::vector<std::vector<float>> sum(K, std::vector<float>(DIM, 0.0f));
-    std::vector<int> count(K, 0);
-
+    std::fill(count.begin(), count.end(), 0);
+    for (auto &s : sum)
+      std::fill(s.begin(), s.end(), 0.0f);
     for (int i = 0; i < data.size(); ++i) {
       int c = assignments[i]; // cluster k
       for (int d = 0; d < DIM; ++d)
@@ -82,6 +85,60 @@ int main(int argc, char *argv[]) {
       }
     }
   }
+
+  // save file
+  ofstream out("ivf.bin", ios::binary);
+
+  if (!out) {
+    cerr << "Error while opening the file for writing";
+    return 1;
+  }
+
+  uint32_t N = static_cast<uint32_t>(data.size());
+
+  // HEADER
+  out.write(reinterpret_cast<const char *>(&K), sizeof(K));
+  out.write(reinterpret_cast<const char *>(&DIM), sizeof(DIM));
+  out.write(reinterpret_cast<const char *>(&N), sizeof(N));
+
+  // CENTROIDS (K * DIM floats)
+  for (const auto &centroid : centroids) {
+    out.write(reinterpret_cast<const char *>(centroid.data()),
+              DIM * sizeof(float));
+  }
+
+  // OFFSETS (prefix-sum dos counts)
+  std::vector<uint32_t> offsets(K + 1);
+  offsets[0] = 0;
+  for (uint32_t j = 0; j < K; ++j) {
+    offsets[j + 1] = offsets[j] + count[j];
+  }
+  out.write(reinterpret_cast<const char *>(offsets.data()),
+            offsets.size() * sizeof(uint32_t));
+
+  // VECTORS ordenados por cluster (via cursor)
+  auto cursor = offsets;
+  std::vector<float> ordered_vectors(N * DIM);
+  std::vector<uint8_t> ordered_labels(N);
+
+  for (uint32_t i = 0; i < N; ++i) {
+    uint32_t cluster_index = assignments[i];
+    uint32_t pos = cursor[cluster_index]++;
+
+    for (uint32_t d = 0; d < DIM; ++d)
+      ordered_vectors[pos * DIM + d] = points[i][d];
+
+    ordered_labels[pos] = labels[i];
+  }
+
+  out.write(reinterpret_cast<const char *>(ordered_vectors.data()),
+            ordered_vectors.size() * sizeof(float));
+
+  // LABELS
+  out.write(reinterpret_cast<const char *>(ordered_labels.data()),
+            ordered_labels.size() * sizeof(uint8_t));
+
+  out.close();
 
   return 0;
 }
